9267d6de93d4b69c26653ec5021519abca241bc4
Daniel McKeown <d@sf3am.com>
2019-03-29 13:11:00 -0700

    add font styling to index page

--- pages/index.js
+++ pages/index.js
@@ -1,92 +1,95 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 import Headernav from "./Headernav";
 
 export default () => (
-<section>
-  <Headernav />
-  <ul id="seis">
-    <li>
-      <Link href='/Objectbrowser'>
-        <a>Objectbrowser (Create)</a>
-      </Link>
-    </li>
-    <li>
-      <Link href='/Desk'>
-        <a>Desk (Read)</a>
-      </Link>
-    </li>
-    <li>
-      <Link href='/Edit'>
-        <a>Edit (Update)</a>
-      </Link>
-    </li>
-    <li>
-      <Link href='/Delete'>
-        <a>Delete (Destroy)</a>
-      </Link>
-    </li>
-    <li>
-      <Link href='/About'>
-        <a>About</a>
-      </Link>
-    </li>
-  </ul>
-  <ul>
-    <li>
-      <Link href='/b' as='/a'>
-        <a>a</a>
-      </Link>
-    </li>
-    <li>
-      <Link href='/a' as='/b'>
-        <a>b</a>
-      </Link>
-    </li>
-    <li>
-      <Link href={{ pathname: '/posts', query: { id: '2' } }} as='/posts/2'>
-        <a>post #2</a>
-      </Link>
-    </li>
-  </ul>
-  <footer id="seisFooter">
-    <a href="https://pacificio.com">pacificIO</a>
-  </footer>
-  <style jsx>{`
-    ul#seis {
-      display: flex;
-    }
-    ul#seis li {
-      width: calc(30px + 20vw);
-      background: yellow;
-      margin-right: calc(1vw + 2px);
-    }
-        .spread {
-          font-family: "Ubuntu Mono", "Inconsolata", "Hack", "Fira Code", Menlo, monospace;
-        }
-        span.valSheetRow {
-          background-color: lightgray;
-          margin-top: 12px;
-          margin-right: 10px;
-          margin-bottom: 0.6rem;
-          line-height: 1.3;
-          padding-left: calc(3px + 1vw);
-          padding-right: calc(1px + 1vw);
-          padding-top: calc(1px + 0.8vw);
-          padding-bottom: calc(1px + 0.6vw);
-        }
-        span.valHeaderRow {
-          background-color: lightblue;
-          margin-top: 12px;
-          margin-right: 10px;
-          margin-bottom: 0.6rem;
-          line-height: 1.3;
-          padding-left: calc(3px + 1vw);
-          padding-right: calc(1px + 1vw);
-          padding-top: calc(1px + 0.8vw);
-          padding-bottom: calc(1px + 0.6vw);
-        }
-        `}</style>
-</section>
-)
+  <section id="homeContainer">
+    <Headernav />
+    <ul id="seis">
+      <li>
+        <Link href="/Objectbrowser">
+          <a>Objectbrowser (Create)</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/Desk">
+          <a>Desk (Read)</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/Edit">
+          <a>Edit (Update)</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/Delete">
+          <a>Delete (Destroy)</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/About">
+          <a>About</a>
+        </Link>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <Link href="/b" as="/a">
+          <a>a</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/a" as="/b">
+          <a>b</a>
+        </Link>
+      </li>
+      <li>
+        <Link href={{ pathname: "/posts", query: { id: "2" } }} as="/posts/2">
+          <a>post #2</a>
+        </Link>
+      </li>
+    </ul>
+    <footer id="seisFooter">
+      <a href="https://pacificio.com">pacificIO</a>
+    </footer>
+    <style jsx>{`
+      ul#seis {
+        display: flex;
+      }
+      ul#seis li {
+        width: calc(30px + 20vw);
+        background: yellow;
+        margin-right: calc(1vw + 2px);
+      }
+      .spread,
+      section#homeContainer, section#homeContainer a {
+        font-family: "Ubuntu Mono", "Inconsolata", "Hack", "Fira Code", Menlo,
+          monospace;
+        color: black;
+      }
+      span.valSheetRow {
+        background-color: lightgray;
+        margin-top: 12px;
+        margin-right: 10px;
+        margin-bottom: 0.6rem;
+        line-height: 1.3;
+        padding-left: calc(3px + 1vw);
+        padding-right: calc(1px + 1vw);
+        padding-top: calc(1px + 0.8vw);
+        padding-bottom: calc(1px + 0.6vw);
+      }
+      span.valHeaderRow {
+        background-color: lightblue;
+        margin-top: 12px;
+        margin-right: 10px;
+        margin-bottom: 0.6rem;
+        line-height: 1.3;
+        padding-left: calc(3px + 1vw);
+        padding-right: calc(1px + 1vw);
+        padding-top: calc(1px + 0.8vw);
+        padding-bottom: calc(1px + 0.6vw);
+      }
+    `}</style>
+  </section>
+);
