328ea4d4d8c07d096e89ae4759daadb4140e0fd8
Daniel McKeown <d@sf3am.com>
2019-03-30 12:35:51 -0700

    remove unused a and b components and update index page

--- pages/index.js
+++ pages/index.js
@@ -35,16 +35,6 @@ export default () => (
     </ul>
     <ul>
       <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-      <li>
         <Link href={{ pathname: "/posts", query: { id: "2" } }} as="/posts/2">
           <a>post #2</a>
         </Link>
